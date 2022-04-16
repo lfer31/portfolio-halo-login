a9b5923b9bdfba890388222fbfc7283510af6cf3
LuisFer <lfer31@gmail.com>
2022-04-15 19:45:19 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        halo: "url('/img/926721.png')"
+      })
+    },
+  },
+  plugins: [],
+}
